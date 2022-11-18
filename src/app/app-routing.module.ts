a9475cede50b57a334254a0a4a28b55c34ba2f4b
Basilevs <stazzzdzr@gmail.com>
2022-11-18 18:44:32 +0300

    pages and routes created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { BasketPageComponent } from './pages/basket-page/basket-page.component';
+import { CatalogPageComponent } from './pages/catalog-page/catalog-page.component';
+import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
 
 const routes: Routes = [
+    {path: 'catalog', component: CatalogPageComponent},
     {path: 'about', component: AboutPageComponent},
+    {path: 'profile', component: ProfilePageComponent},
+    {path: 'basket', component: BasketPageComponent},
 ];
 
 
